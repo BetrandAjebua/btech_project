d9d06a8827dcb07d9e5a68619cf22ccdd70741e6
Betrand Ajebua <betrandajebua@gmail.com>
2023-05-17 13:15:59 +0100

    admin page well connected login

--- admin_section/alogin copy.php
+++ admin_section/alogin copy.php
@@ -1,9 +1,10 @@
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <link rel="stylesheet" href="login.css">
-    <link rel="stylesheet" href="login1.css">
+    <link rel="stylesheet" href="reg.css">
     <link rel="stylesheet" href="../bootstrap-5.2.3-dist/bootstrap-5.2.3-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="../jquery-ui-1.13.2.custom/jquery-ui-1.13.2.custom/jquery-ui.structure.css">
     <link rel="stylesheet" href="../jquery-ui-1.13.2.custom/jquery-ui-1.13.2.custom/jquery-ui.theme.css">
@@ -13,29 +14,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BBMS-login</title>
 </head>
+
 <body class="container-fluid bg-dark">
     
 
     <!--LOGIN FORM FOR THE DONOR-->
     <div class="form-con text-danger col-9 bg-white">
-        <form action="" >
-            <h1 class="text-center mb-5">Donor Login</h1>
+        <form action="SUBMIT.PHP" method="post" >
+            <p><i>system will be locked if you enter Wrong password more than 5 times</i></p>
+            <h1 class="text-center mb-5">Admin Login</h1>
 
             <!--NAME SECTION-->
 
             <div class="container-alignment">
                 <div class="f-name">
-                    <label for="f-name" class="l-input">Full Names</label>
-                    <input type="text" name="f-name" id="f-name" class="I-input" required>
+                    <input type="text" name="f-name" id="f-name"  placeholder="Username" class="I-input" required>
                 </div>
+                <br>
                 <br>
                 <div class="password">
-                    <label for="l-name" class="l-input">Password</label>
-                    <input type="password" name="password" id="l-name" class="I-input" required>
+
+                    <input type="password" name="password" placeholder="Enter Password" id="l-name" class="I-input" required>
                 </div>
                 <br>
-                <button type="submit" class="bg-danger text-white h6 p-1" >Submit</button>
-                <a href="register.html" class="text-danger reg-log">Register?</a>
+                <button type="submit" class="bg-white text-danger rounded-3 h6 p-1" >Submit</button>
                 <br>
             </div>
 
