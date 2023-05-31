66a23dfe8b01d1efcaf0e4799bd6b4f27bddf8f9
Betrand Ajebua <betrandajebua@gmail.com>
2023-05-31 16:07:43 +0100

    aDMIN SECTION STARTESD WITH THE INDEX ADMIN PAGE

--- assets/connection/head.php
+++ assets/connection/head.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Blood Bank Managwment</title>
+  <title>Blood Bank Management</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
