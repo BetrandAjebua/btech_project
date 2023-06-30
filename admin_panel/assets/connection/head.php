e06dafbf6004290aafd33eea1d410483b871eb1c
Betrand Ajebua <betrandajebua@gmail.com>
2023-06-30 17:48:16 +0100

    slashed removed

--- admin_panel/assets/connection/head.php
+++ admin_panel/assets/connection/head.php
@@ -1,5 +1,5 @@
 <?php require "connect.php";?>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">\
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/dist/fonts/bootstrap-icons.woff">
 
 
