9edb331db0464a28eac7505601b1e57352890cc6
Betrand Ajebua <betrandajebua@gmail.com>
2023-06-30 11:26:52 +0100

    item deleted

--- admin_panel/pages/action.php
+++ admin_panel/pages/action.php
@@ -1,25 +1,3 @@
-<?php
-
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
-    use PHPMailer\PHPMailer\OAuthTokenProvider;
-
-    
-    /* Exception class. */
-    require '../PHPMailer-master/src/Exception.php';
-    /* The main PHPMailer class. */
-    require '../PHPMailer-master/src/PHPMailer.php';
-    /* SMTP class, needed if you want to use SMTP. */
-    require '../PHPMailer-master/src/SMTP.php';
-    require '../PHPMailer-master/src/OAuth.php';
-    // require '../PHPMailer-master/src/OAuthTokenProvider.php';
-    
-    /* ... */
-    
-
-?>
-
-
 <?php    
 
 require "../assets/connection/connect.php";
